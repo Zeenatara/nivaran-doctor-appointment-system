6e62d1e64591be38341f77142575547794648f5e
Zeenat Ara <aec.ece.zeenatara.2027.082@gmail.com>
2026-04-27 23:04:58 +0530

    Update routing and UI for About and Contact pages

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -10,23 +10,27 @@ import Appointment from './pages/Appointment';
 import Navbar from './components/Navbar';
 import Banner from './components/Banner';
 import Footer from './components/Footer';
+import About from './pages/About';
 
 const App = () => {
   return (
     <>
-    <Navbar/>
+      <div className="px-4 sm:px-10 md:px-14 lg:px-20">
+        <Navbar />
 
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/doctors" element={<Doctors />} />
-        <Route path="/contact" element={<Contact />} />
-        <Route path="/doctors/:speciality" element={<Doctors />} />
-        <Route path="/login" element={<Login />} />
-        <Route path="/my-profile" element={<MyProfile />} />
-        <Route path="/my-appointment" element={<MyAppointment />} />
-        <Route path="/appointment/:docId" element={<Appointment />} />
-      </Routes>
-      <Footer/>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/doctors" element={<Doctors />} />
+          <Route path="/contact" element={<Contact />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/doctors/:speciality" element={<Doctors />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/my-profile" element={<MyProfile />} />
+          <Route path="/my-appointment" element={<MyAppointment />} />
+          <Route path="/appointment/:docId" element={<Appointment />} />
+        </Routes>
+        <Footer />
+      </div>
     </>
   );
 }
