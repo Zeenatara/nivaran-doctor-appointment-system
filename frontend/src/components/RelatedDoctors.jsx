b153662f7400e810a6f1319dd92740fc6d5a52b6
Zeenat Ara <aec.ece.zeenatara.2027.082@gmail.com>
2026-04-27 23:04:29 +0530

    Add RelatedDoctors component

---
+++ frontend/src/components/RelatedDoctors.jsx
@@ -0,0 +1,64 @@
+import React, { useContext, useEffect, useState } from 'react';
+import { useNavigate } from 'react-router-dom';
+import { AppContext } from '../context/AppContext';
+
+const RelatedDoctors = ({ docId, speciality }) => {
+
+
+  const {doctors}=useContext(AppContext);
+
+  const navigate=useNavigate();
+
+  const[relDocs,setRelDocs]=useState([]);
+  useEffect(()=>{
+    if(doctors.length>0 && speciality){
+      const related=doctors.filter((doc)=>doc.speciality===speciality && doc._id!==docId);
+      setRelDocs(related);
+    }
+
+  },[doctors,speciality,docId]);
+  return (
+    <div className="flex flex-col items-center gap-4 my-16 text-gray-900 md:mx-10">
+      <h1 className="text-3xl font-medium">Top Doctors to Book</h1>
+
+      <p className="sm:w-1/3 text-center text-sm">
+        Simply browse through our extensive list of trusted doctors.
+      </p>
+
+      <div className="w-full grid grid-cols-[repeat(auto-fill,minmax(200px,1fr))] gap-4 pt-5 gap-y-6 px-3 sm:px-0">
+        {doctors.slice(0, 5).map((item) => (
+          <div
+            key={item._id}
+    onClick={() => {navigate(`/appointment/${item._id}`); scrollTo(0, 0)}}
+            className="border border-[#70aab2] rounded-xl overflow-hidden cursor-pointer hover:translate-y-[10px] transition-all duration-500"
+          >
+            <img className="bg-[#70aab2]" src={item.image} alt={item.name} />
+
+            <div className="p-4">
+              <div className="flex items-center gap-2 text-sm text-green-500">
+                <p className="w-2 h-2 bg-green-500 rounded-full"></p>
+                <p>Available</p>
+              </div>
+
+              <p className="text-gray-900 text-lg font-medium">{item.name}</p>
+
+              <p className="text-gray-600 text-sm">{item.speciality}</p>
+            </div>
+          </div>
+        ))}
+      </div>
+
+      <button
+        onClick={() => {
+          navigate('/doctors');
+          scrollTo(0, 0);
+        }}
+        className="bg-[#70aab2] text-slate-700 text-lg font-medium px-8 py-2 mt-4 rounded-full"
+      >
+        More
+      </button>
+    </div>
+  );
+}
+
+export default RelatedDoctors;
