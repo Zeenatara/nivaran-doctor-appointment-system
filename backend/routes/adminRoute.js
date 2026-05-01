00042eb1422fc7c09a60b4df447f448290fda559
Zeenat Ara <aec.ece.zeenatara.2027.082@gmail.com>
2026-05-02 02:28:44 +0530

    Integrate backend components and complete initial setup

---
+++ backend/routes/adminRoute.js
@@ -0,0 +1,11 @@
+import express from 'express';
+import { addDoctor } from '../controllers/adminController.js';
+import upload from '../middleware/multer.js';
+
+
+const adminRoute = express.Router();
+
+adminRoute.post("/add-doctor", upload.single("image"), addDoctor);
+
+
+export default adminRoute;
