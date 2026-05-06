293cfbcdda8e10d52719f25537a041a401b6911c
Zeenat Ara <aec.ece.zeenatara.2027.082@gmail.com>
2026-05-07 01:06:59 +0530

    protect admin routes with authentication middleware

--- backend/routes/adminRoute.js
+++ backend/routes/adminRoute.js
@@ -1,11 +1,12 @@
 import express from 'express';
 import { addDoctor,adminLogin } from '../controllers/adminController.js';
 import upload from '../middleware/multer.js';
+import authAdmin from '../middleware/authAdmin.js';
 
 
 const adminRoute = express.Router();
 
-adminRoute.post("/add-doctor", upload.single("image"), addDoctor);
+adminRoute.post("/add-doctor",authAdmin, upload.single("image"), addDoctor);
 adminRoute.post("/login", adminLogin);
 
 export default adminRoute;
