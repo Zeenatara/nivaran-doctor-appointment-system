6bcba6092e90c897aa50b54ed902baf54583ada0
Zeenat Ara <aec.ece.zeenatara.2027.082@gmail.com>
2026-05-10 20:56:55 +0530

    setup core admin application structure

---
+++ admin/src/main.jsx
@@ -0,0 +1,23 @@
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import { BrowserRouter } from 'react-router-dom'
+import './index.css'
+import App from './App.jsx'
+import AppContextProvider from './context/AppContext.jsx'
+
+import AdminContextProvider from './context/AdminContext.jsx'
+import DoctorContextProvider from './context/DoctorContext.jsx'
+
+
+createRoot(document.getElementById('root')).render(
+  <BrowserRouter>
+  <AdminContextProvider>
+    <DoctorContextProvider>
+      <AppContextProvider>
+        <App />
+      </AppContextProvider>
+    </DoctorContextProvider>
+  </AdminContextProvider>
+
+  </BrowserRouter>
+);
